ad8253b4ec33b412898ccb933de5480a2911484c
Andreas Gropp <starmaerker@googlemail.com>
2020-04-29 20:59:44 +0200

    add index.html

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, jsonify, request
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
